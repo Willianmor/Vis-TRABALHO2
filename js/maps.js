2c40ead5443ee46c9fde6f3aee72e422136ac005
willianmor <willian_coutinho@id.uff.br>
2020-11-12 17:21:41 -0300

    tentativa - ainda não funcionou

--- js/maps.js
+++ js/maps.js
@@ -12,6 +12,7 @@ export class Maps {
         this.projection = null;
         this.path = null;
         this.data = null;
+        this.zoom =null;
 
         this.createSvg();
         this.createMargins();
@@ -33,6 +34,7 @@ export class Maps {
             .attr("transform", `translate(${this.config.left},${this.config.top})`)
     }
 
+    
     // LEER: https://stackoverflow.com/questions/33087405/using-queue-to-load-multiple-files-and-assign-to-globals
     async render(myfile) {
         // Map and projection
@@ -60,12 +62,16 @@ export class Maps {
                 )
                 .style("stroke", "black")
                 .style("opacity", .3)
-                
+            
+            zoomed()
+            
         }.bind(this));
+
+        
 
         // //zoom
         // var g = this.svg
-        // var zoom = d3.zoom()
+        // this.zoom = d3.zoom()
         //     .scaleExtent([1, 10])
         //     .on('zoom', function() {
         //         g.append("g")
@@ -78,7 +84,39 @@ export class Maps {
         //                 console.log("chegou2")
         // });
 
-        // g.call(zoom);
+        // this.svg.call(this.zoom);
+
+
+        //zoom map
+        function zoomed(){  
+                        
+            this.svg.append("g")
+            .selectAll("path")
+            .data(data.features)
+            .enter()
+            .append("path")
+            .attr("transform", d3.event.transform);
+            console.log(d3.event.transform);
+        } 
+    
+        this.zoom = d3.zoom()
+            .scaleExtent([1, 8])
+            .translateExtent([[0,0], [this.config.width, this.config.height]])
+            .extent([[0, 0], [this.config.width, this.config.height]])
+            .on("zoom", zoomed);
+        
+        this.svg.append("g")
+            .attr('class','zoom')
+            .attr("width", this.config.width)
+            .attr("height", this.config.height)
+            .call(this.zoom);
+
+        console.log("chegou")
+        //bibliografias:
+        //https://bl.ocks.org/d3noob/c056543aff74a0ac45bef099ee6f5ff4
+        //https://stackoverflow.com/questions/35357164/d3-zoom-cannot-read-property-apply-of-undefined/35357269
+        //importante: https://stackoverflow.com/questions/21550534/set-d3-zoom-scaleextent-to-positive-values
+
     }
 
 }
